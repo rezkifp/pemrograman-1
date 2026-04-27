18166b66d88e0d6719d017586ba9d068be4de661
rezkifp <139482619+rezkifp@users.noreply.github.com>
2026-04-27 08:07:45 +0700

    Add files via upload

---
+++ p2_rezki/src/latihan1_pert2/Latihan1.java
@@ -0,0 +1,11 @@
+import java.util. Scanner;
+    public class Latihan1 {
+        public static void main(String args[]) {
+
+        Scanner input = new Scanner
+        (System.in);
+        Scanner nama;   int usia;
+        System.wt.print("input nama:")
+        nama=input.nextLine();
+        }
+        }
